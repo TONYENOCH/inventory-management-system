f75a8542fc6b244f6ebf765b29f5960590522d92
Amedu Tony <tonyamedu66@gmail.com>
2023-05-02 20:44:25 +0100

    first

--- my_venv/Scripts/activate.bat
+++ my_venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\WENG DAVO\Desktop\rest\my_venv
+set VIRTUAL_ENV=C:\Users\TONY PC\Desktop\inventory\crispy-inventory\my_venv
 
 if not defined PROMPT set PROMPT=$P$G
 
